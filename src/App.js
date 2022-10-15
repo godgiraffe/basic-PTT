9df890a9d54d848c0089d4737206bc34593df4db
roy_chen <roy_chen@gss.com.tw>
2022-10-15 12:29:05 +0800

    feat: add theme provider

--- src/App.js
+++ src/App.js
@@ -4,21 +4,41 @@ import Layout from "./pages/Layout";
 import BoardList from "./pages/BoardList/BoardList";
 import ArticleList from "./pages/ArticleList/ArticleList";
 import ArticleContent from "./pages/ArticleContent/ArticleContent";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  default: {
+    devices: {
+      mobile: {
+        mediaQuery: "max-width: 485px",
+      },
+      tablet: {
+        mediaQuery: "max-width: 960px",
+      }
+    },
+  },
+};
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<BoardList />}></Route>
-          <Route path="/ArticleList/:boardName" element={<ArticleList />}></Route>
-          <Route
-            path="/ArticleContent/:boardName/:ArticleId"
-            element={<ArticleContent />}
-          ></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme.default}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<BoardList />}></Route>
+            <Route
+              path="/ArticleList/:boardName"
+              element={<ArticleList />}
+            ></Route>
+            <Route
+              path="/ArticleContent/:boardName/:ArticleId"
+              element={<ArticleContent />}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
